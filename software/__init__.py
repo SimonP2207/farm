c0e45a5256a63aa6f863b96358c9cf792feaed82
SimonP2207 <simon.purser@skao.int>
2022-10-14 09:51:56 +0100

    Update farm.software.__init__.py
    - Previously farm.software.__init__.py checked that miriad, casa and oskar were present in your path. This would lead to Errors being thrown if any weren't in PATH. However, the responsibility for checking for each of these software packages is in PATH has now been delegated to farm.software.miriad, farm.software.casa and farm.software.oskar, so that Errors are only raised should those software packages be used.

--- software/__init__.py
+++ software/__init__.py
@@ -3,7 +3,3 @@ from .common import which
 from . import oskar
 from .wsclean import wsclean
 
-# Check installations of casa, miriad and oskar
-for program in ('miriad', 'casa', 'oskar'):
-    if which(program) is None:
-        raise SystemError(f"{program} not in PATH")
